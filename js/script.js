4e1933f1a250aa0296dd643b960f3d78ce50b411
Omar García Suárez <ogs75@yahoo.es>
2024-01-21 20:46:37 +0100

    primer commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function openModal() {
+  document.getElementById("modalOverlay").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modalOverlay").style.display = "none";
+}
+
+function sendForm(){
+  alert("Se ha sercervado su cita correctamente!")
+}
